474234f156e88b30b9dde95729a2c536336fc3e7
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-01-03 15:18:50 -0900

    add course base fields
    Co-authored-by: Jeff Lai <pjefflai@gmail.com>
    Co-authored-by: Jeff Terrell <jeffaterrell@gmail.com>

---
+++ UniversityRegistrar/Models/Course.cs
@@ -0,0 +1,17 @@
+//course name, course number
+using System.Collections.Generic;
+
+namespace Registrar.Models
+{
+  public class Course
+  {
+    public Course()
+    {
+      this.JoinEntities = new HashSet<CourseStudent>();
+    }
+    public int CourseId {get; set;}
+    public string Name {get; set;}
+    public int CourseNumber {get; set;}
+    public virtual ICollection<CourseStudent> JoinEntities {get; set;}
+  }
+}
